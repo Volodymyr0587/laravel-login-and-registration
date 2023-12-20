45491121e65a6d64a35d37a351420536ec800831
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-20 16:33:05 +0200

    frontend

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('includes.hero')
+
+@include('includes.feature')
+
+@include('includes.testimonials')
+
+@include('includes.cta')
+
+@endsection
